442ffd78ab2c965de69cc841d9c1f44bab91b6fc
azy64 <allysaidi64@gmail.com>
2022-02-23 19:48:09 +0100

    Add button for fetching greetings

--- src/component/Greeting.js
+++ src/component/Greeting.js
@@ -10,9 +10,25 @@ const Greeting = () => {
   const message = useSelector((state) => state.messageReducer.message);
   return (
     <div>
-      Message:
-      {' '}
-      {message.text}
+      <div style={{ marginLeft: '10px', margin: 'auto', width: '300px' }}>
+        Message:
+        {message.text}
+        {' '}
+        <br />
+        <button
+          type="button"
+          onClick={() => dispatch(fetchmessage())}
+          style={{
+            border: 'none',
+            backgroundColor: '#007bff',
+            padding: '6px',
+            borderRadius: '10px',
+            color: 'white',
+          }}
+        >
+          Greet
+        </button>
+      </div>
     </div>
   );
 };
